a8d1f4e05f86c13b128b8f92c08a55895db22c86
Enrique Ruvalcaba <jordison352@gmail.com>
2023-01-31 14:19:57 -0600

    Setting the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+import { Card, NavCard} from "./components/Cards";
+
+function App() {
+  return <div className="App">
+    <NavCard />
+    <Card />    
+  </div>;
+}
+
+export default App;
---
+++ src/components/Cards/NavCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NavCard = () => {
+  return (
+    <div>NavCard</div>
+  )
+}
